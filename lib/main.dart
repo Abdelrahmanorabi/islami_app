77a8705e63794b6390f558af8743f68eb5a21a0b
AbdelrahmanDev <abdelrahmanoraby101@gmail.com>
2022-08-08 02:37:52 +0200

    implement localization into project

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:islami_app/home/hadeth/hadeth_details_screen.dart';
 import 'package:islami_app/home/home_screen.dart';
 import 'package:islami_app/home/quran/sura_details.dart';
-
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'my_theme.dart';
 
 void main() {
@@ -15,6 +16,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         theme:MyTheme.lightTheme,
+        localizationsDelegates: AppLocalizations.localizationsDelegates,
+        supportedLocales: AppLocalizations.supportedLocales,
         initialRoute: HomeScreen.routeName,
         routes: {
           HomeScreen.routeName: (_) => HomeScreen(),
