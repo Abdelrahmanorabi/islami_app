1f15b9a135f7ae2e390a06627e2657968752880a
AbdelrahmanDev <abdelrahmanoraby101@gmail.com>
2022-08-10 15:38:57 +0200

    finish implementation  of  shared prefrances for all app

--- lib/home/providers/settings_provider.dart
+++ lib/home/providers/settings_provider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SettingsProvider extends ChangeNotifier {
   /*=====================[Variables]======================*/
@@ -9,28 +10,37 @@ class SettingsProvider extends ChangeNotifier {
 
   /*=====================[Methods]======================*/
 
-  void changeLanguage(String newLanguage) {
+  void changeLanguage(String newLanguage) async {
+    final prefs = await SharedPreferences.getInstance();
     if (newLanguage == currentLanguage) {
       return;
     }
     currentLanguage = newLanguage;
+    prefs.setString('lang', currentLanguage);
     notifyListeners();
   }
 
-  void changeTheme(ThemeMode newTheme) {
+  void changeTheme(ThemeMode newTheme) async {
+    final prefs = await SharedPreferences.getInstance();
     if (newTheme == currentTheme) {
       return;
     }
     currentTheme = newTheme;
+    prefs.setString(
+        'theme', (currentTheme == ThemeMode.light) ? 'light' : 'dark');
     notifyListeners();
   }
 
   bool isDarkMode() {
     return (currentTheme == ThemeMode.dark);
   }
-  String getMainBackground(){
-    return isDarkMode()? 'assets/images/background_pattern_dark.png':'assets/images/background_pattern.png';
+
+  String getMainBackground() {
+    return isDarkMode()
+        ? 'assets/images/background_pattern_dark.png'
+        : 'assets/images/background_pattern.png';
   }
+
 /*===================================================*/
 
 }
--- lib/main.dart
+++ lib/main.dart
@@ -5,28 +5,48 @@ import 'package:islami_app/home/home_screen.dart';
 import 'package:islami_app/home/providers/settings_provider.dart';
 import 'package:islami_app/home/quran/sura_details.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
 import 'my_theme.dart';
 
 void main() {
-  runApp(ChangeNotifierProvider <SettingsProvider>(
+  runApp(ChangeNotifierProvider<SettingsProvider>(
       create: (BuildContext context) {
-          return SettingsProvider();
+        return SettingsProvider();
       },
       child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
+  /*===========================[Variables]=============================*/
+  late SettingsProvider settingProvider;
+
+  /*==================================================================*/
+  /*===========================[Methods]=============================*/
+  void getSharedPreferences() async {
+    final prefs = await SharedPreferences.getInstance();
+
+    // set default language
+    settingProvider.changeLanguage(prefs.getString('lang') ?? 'ar');
+
+    // get default theme
+    if (prefs.getString('theme') == 'light') {
+      settingProvider.changeTheme(ThemeMode.light);
+    } else if (prefs.getString('theme') == 'dark') {
+      settingProvider.changeTheme(ThemeMode.dark);
+    }
+  }
+  /*==================================================================*/
 
   @override
   Widget build(BuildContext context) {
-    var settingProvider = Provider.of<SettingsProvider>(context);
-
-
+    settingProvider = Provider.of<SettingsProvider>(context);
+    getSharedPreferences();
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: MyTheme.lightTheme,
         darkTheme: MyTheme.darkTheme,
-        themeMode:settingProvider.currentTheme ,
+        themeMode: settingProvider.currentTheme,
         localizationsDelegates: AppLocalizations.localizationsDelegates,
         supportedLocales: AppLocalizations.supportedLocales,
         locale: Locale(settingProvider.currentLanguage),
