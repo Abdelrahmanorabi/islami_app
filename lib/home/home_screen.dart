1c9716be6a86da0dafe9eb567b8beb9fc30fc991
AbdelrahmanDev <abdelrahmanoraby101@gmail.com>
2022-08-10 10:27:01 +0200

    implement setting tab implement theme implement state management using provider

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:islami_app/home/hadeth/hadeth_tab.dart';
+import 'package:islami_app/home/providers/settings_provider.dart';
 import 'package:islami_app/home/quran/quran_tab.dart';
 import 'package:islami_app/home/radio/radio_tab.dart';
+import 'package:islami_app/home/settings/setting_tab.dart';
 import 'package:islami_app/home/tasbeh/sebha_tab.dart';
-import 'package:islami_app/my_theme.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:provider/provider.dart';
 
 class HomeScreen extends StatefulWidget {
   static const String routeName = 'home';
@@ -16,54 +18,71 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   /*===================[Variables]==========================*/
   int selectedIndex = 0;
-  List<Widget> tabs = [QuranTab(), HadethTab(), SebhaTab(), RadioTab()];
+  List<Widget> tabs = [
+    QuranTab(),
+    HadethTab(),
+    SebhaTab(),
+    RadioTab(),
+    SettingsTab()
+  ];
 
   /*========================================================*/
   @override
   Widget build(BuildContext context) {
+    var settingProvider = Provider.of<SettingsProvider>(context);
     return SafeArea(
       child: Container(
-        decoration: const BoxDecoration(
+        decoration:  BoxDecoration(
             image: DecorationImage(
                 fit: BoxFit.fill,
                 image: AssetImage(
-                  'assets/images/background_pattern.png',
+                  settingProvider.getMainBackground(),
                 ))),
         child: Scaffold(
-          appBar: AppBar(
-            title: Text(AppLocalizations.of(context)!.app_title)),
-          bottomNavigationBar: Theme(
-            data: Theme.of(context).copyWith(canvasColor: MyTheme.primaryColor),
-            child: BottomNavigationBar(
-              currentIndex: selectedIndex,
-              onTap: (int index) {
-                selectedIndex = index;
-                setState(() {});
-              },
-              items:  [
-                BottomNavigationBarItem(
-                  icon: const ImageIcon(AssetImage('assets/images/ic_quran.png')),
-                  label: AppLocalizations.of(context)!.quran_title,
-                ),
-                BottomNavigationBarItem(
-                  icon: const ImageIcon(AssetImage('assets/images/ic_hadeth.png')),
-                  label:  AppLocalizations.of(context)!.hadeth_title,
-                ),
-                BottomNavigationBarItem(
-                  icon: const ImageIcon(AssetImage('assets/images/ic_sebha.png')),
-                  label:  AppLocalizations.of(context)!.tasbeh_title,
-                ),
-                BottomNavigationBarItem(
-                  icon: const ImageIcon(AssetImage('assets/images/ic_radio.png')),
-                  label:  AppLocalizations.of(context)!.radio_title,
-                ),
-              ],
-            ),
+          appBar: AppBar(title: Text(AppLocalizations.of(context)!.app_title)),
+          bottomNavigationBar: BottomNavigationBar(
+            currentIndex: selectedIndex,
+            onTap: (int index) {
+              selectedIndex = index;
+              setState(() {});
+            },
+            items: [
+              BottomNavigationBarItem(
+                backgroundColor:
+                    Theme.of(context).bottomNavigationBarTheme.backgroundColor,
+                icon: const ImageIcon(AssetImage('assets/images/ic_quran.png')),
+                label: AppLocalizations.of(context)!.quran_title,
+              ),
+              BottomNavigationBarItem(
+                backgroundColor:
+                    Theme.of(context).bottomNavigationBarTheme.backgroundColor,
+                icon:
+                    const ImageIcon(AssetImage('assets/images/ic_hadeth.png')),
+                label: AppLocalizations.of(context)!.hadeth_title,
+              ),
+              BottomNavigationBarItem(
+                backgroundColor:
+                    Theme.of(context).bottomNavigationBarTheme.backgroundColor,
+                icon: const ImageIcon(AssetImage('assets/images/ic_sebha.png')),
+                label: AppLocalizations.of(context)!.tasbeh_title,
+              ),
+              BottomNavigationBarItem(
+                backgroundColor:
+                    Theme.of(context).bottomNavigationBarTheme.backgroundColor,
+                icon: const ImageIcon(AssetImage('assets/images/ic_radio.png')),
+                label: AppLocalizations.of(context)!.radio_title,
+              ),
+              BottomNavigationBarItem(
+                backgroundColor:
+                    Theme.of(context).bottomNavigationBarTheme.backgroundColor,
+                icon: Icon(Icons.settings),
+                label: AppLocalizations.of(context)!.setting_title,
+              ),
+            ],
           ),
           body: tabs[selectedIndex],
         ),
       ),
     );
-
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,40 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:islami_app/home/hadeth/hadeth_details_screen.dart';
 import 'package:islami_app/home/home_screen.dart';
+import 'package:islami_app/home/providers/settings_provider.dart';
 import 'package:islami_app/home/quran/sura_details.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:provider/provider.dart';
 import 'my_theme.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(ChangeNotifierProvider <SettingsProvider>(
+      create: (BuildContext context) {
+          return SettingsProvider();
+      },
+      child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
+    var settingProvider = Provider.of<SettingsProvider>(context);
+
+
     return MaterialApp(
         debugShowCheckedModeBanner: false,
-        theme:MyTheme.lightTheme,
+        theme: MyTheme.lightTheme,
+        darkTheme: MyTheme.darkTheme,
+        themeMode:settingProvider.currentTheme ,
         localizationsDelegates: AppLocalizations.localizationsDelegates,
         supportedLocales: AppLocalizations.supportedLocales,
+        locale: Locale(settingProvider.currentLanguage),
         initialRoute: HomeScreen.routeName,
         routes: {
           HomeScreen.routeName: (_) => HomeScreen(),
           SuraDetailsScreen.routeName: (_) => SuraDetailsScreen(),
-          HadethDetailsScreen.routeName:(_)=> HadethDetailsScreen(),
+          HadethDetailsScreen.routeName: (_) => HadethDetailsScreen(),
         });
   }
 }
